397c2e0f71c784858e6f254b66895ea081d49550
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-09 01:45:13 +0300

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,90 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Venocyber_tech,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function VENOCYBER_MD_BOT_PAIRING_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Venocyber_Tech = Venocyber_Tech({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Gifted_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id, { text: 'VENOCYBER-MD-BOT;;;' + b64data });
+	
+				   let VENOCYBER_MD_BOT_TEXT = `
+*_Qr Code By Gifted Tech_*
+*_Made With 🤍_*
+	
+_Don't Forget To Give Star To My Repo_`
+	 await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id,{text:VENOCYBER_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Venocyber_Tech.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					VENOCYBER_MD_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await VENOCYBER_MD_BOT_QR_CODE()
+});
+module.exports = router
