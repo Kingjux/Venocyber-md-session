9b490ef6d479877bb042f27c0230d50fc31077e4
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-03-16 06:29:35 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id, { text: 'VENOCYBER-MD-BOT;;;' + b64data });
+				   let session = await Qr_Code_By_Venocyber_Tech.sendMessage(Qr_Code_By_Venocyber_Tech.user.id, { text: 'VENOCYBER-MD;;;' + b64data });
 	
 				   let VENOCYBER_MD_TEXT = `
 *_Qr Code By Venocyber Tech_*
